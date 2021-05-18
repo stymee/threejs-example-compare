3ffe94b37e6898eb1267f0ff5e4f8dea73200587
David Howard <david_howard@goodyear.com>
2021-05-18 17:02:10 -0400

    testing sveltekit options

--- sveltekit-threejs-example/svelte.config.js
+++ sveltekit-threejs-example/svelte.config.js
@@ -2,15 +2,23 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: '#svelte',
-		//ssr: false,
-	}
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        // target: '#svelte',
+        //ssr: false,
+        // ssr: {
+        //     noExternal: Object.keys(pkg.dependencies || {}),
+        // },
+        // vite: {
+        //     optimizeDeps: {
+        //         include: ['three.js'],
+        //     },
+        // }
+    },
 };
 
 export default config;
